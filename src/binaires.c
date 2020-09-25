7d1cbb969d66852b512e9d702004f935a4a22087
n0way <n0way@localhost.localdomain>
2020-09-25 12:41:54 +0200

    alloc

--- src/binaires.c
+++ src/binaires.c
@@ -1,15 +1,21 @@
-#include <stdlib.h>
+#include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static unsigned long long X = 123456ULL;
 
+const uint64_t a = 0x5DEECE66D;
+const uint64_t c = 0xB;
+const unsigned long m = (1UL << 48);
+
 unsigned char crand48(void) {
-    /* Insérez votre code ici. */
-    return 0;
+    /* See https://nccbulletin.ru/files/article/kalgin_0.pdf */
+    X = (X * a + c) & (m - 1);
+    return (X >> (8 * 4)) & 0xff;
 }
 
-int main(void)
-{
+int main(void) {
+    printf("a:%lu c:%lu m:%lu\n", a, c, m);
     printf("%hhu\n", crand48());
     printf("%hhu\n", crand48());
     printf("%hhu\n", crand48());
--- src/elempool.c
+++ src/elempool.c
@@ -18,19 +18,33 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "elempool.h"
+
 #include <stdlib.h>
 #include <string.h>
-#include "elempool.h"
+
 #include "bitset1000.h"
 
-static unsigned char *memoire_elem_pool = 0;
+#define n (1000)
+
+/* Point to the start of element pool */
+static struct elem *memoire_elem_pool = 0;
 
 /**
    Fonction allouant un element
+   ./ensitestgc --all
 */
 struct elem *alloc_elem(void) {
-    /* ajouter votre code ici / add your code here */
-    return 0;
+    static size_t i = 0;
+    while (i < n && bt1k_get(i)) {
+        i++;
+    }
+    /* Problème s i plus de 1000 appels à alloc_elem */
+    if (i == n) {
+        return NULL;
+    }
+    bt1k_set(i, true);
+    return &memoire_elem_pool[i];
 }
 
 /**
@@ -41,10 +55,13 @@ void gc_elems(struct elem **heads, int nbheads) {
     /* ajouter votre code ici / add your code here */
 }
 
+/**
+ * Intialisation de memoire_elem_pool 
+*/
 void init_elems() {
     bt1k_reset();
     if (memoire_elem_pool == NULL) {
-        memoire_elem_pool = malloc(1000 * sizeof(struct elem));
+        memoire_elem_pool = malloc(n * sizeof(struct elem));
     }
-    memset(memoire_elem_pool, 0, 1000 * sizeof(struct elem));
+    memset(memoire_elem_pool, 0, n * sizeof(struct elem));
 }
